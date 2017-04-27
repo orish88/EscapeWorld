b1d4c5708b6748480d93accf3ddbbb8d17b9781d
YaelB <be.yael.10@gmail.com>
2017-04-27 17:37:03 +0300

    fsec

--- app/src/main/java/com/sh/ori/escapeworld/WelcomeActivity.java
+++ app/src/main/java/com/sh/ori/escapeworld/WelcomeActivity.java
@@ -2,12 +2,15 @@ package com.sh.ori.escapeworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class Register extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_welcome);
+        Log.d("notes","startMenu on create called");
+
     }
 }
--- app/src/main/java/com/sh/ori/escapeworld/loginActivity.java
+++ app/src/main/java/com/sh/ori/escapeworld/loginActivity.java
@@ -3,11 +3,11 @@ package com.sh.ori.escapeworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 }
